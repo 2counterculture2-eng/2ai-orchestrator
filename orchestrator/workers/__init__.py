2640fd3ccb2b4fb91c9d8e0832d005bcb87115be
2counterculture2-eng <2counterculture2@gmail.com>
2026-06-14 08:41:08 +0900

    feat: export GmoCoinWorker and BitgetWorker

--- orchestrator/workers/__init__.py
+++ orchestrator/workers/__init__.py
@@ -1,5 +1,7 @@
 from .base_worker import BaseWorker, TaskResult
 from .translation_worker import TranslationWorker
 from .trading_worker import TradingWorker
+from .gmo_coin_worker import GmoCoinWorker
+from .bitget_worker import BitgetWorker
 
-__all__ = ["BaseWorker", "TaskResult", "TranslationWorker", "TradingWorker"]
+__all__ = ["BaseWorker", "TaskResult", "TranslationWorker", "TradingWorker", "GmoCoinWorker", "BitgetWorker"]
